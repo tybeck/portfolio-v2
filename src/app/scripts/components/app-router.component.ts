cabe0b52e9a01e8baebcda14a362ea73551b4bc1
Tyler Beck <y2xtreme600@yahoo.com>
2016-08-13 19:01:40 -0400

    Router wrapper to alllow better styling. - Release v2.0.13

---
+++ src/app/scripts/components/app-router.component.ts
@@ -0,0 +1,26 @@
+'use strict';
+
+import { Component } from '@angular/core';
+import { ROUTER_DIRECTIVES }  from '@angular/router';
+
+@Component({
+
+  'selector': 'app-router',
+
+  'templateUrl': 'views/components/app-router.html',
+
+  'styleUrls': [
+
+    'styles/components/app-router.component.css'
+
+  ],
+
+  'directives': [
+    ROUTER_DIRECTIVES
+  ]
+
+})
+
+export class AppRouterComponent {
+
+};
